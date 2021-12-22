438f6d4b39eab9c5e9d476ca5ec91d674fa3a2db
Tony Swearingen <unlimited1974@aol.com>
2021-12-22 17:39:29 -0600

    code pushed

---
+++ index.js
@@ -0,0 +1,14 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+const string = "Hello!";
+console.log(string.toUpperCase());
+console.log(string.toLowerCase());
+
+function shoutHELLO() {
+    console.log("HELLO!")
+}
