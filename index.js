f6d9458327f47126c46af700b7ebc3ddb635db1f
Tony Swearingen <unlimited1974@aol.com>
2021-12-23 09:58:09 -0600

    pushed code

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ function shout(string) {
 function whisper(string) {
     return string.toLowerCase();
 }
-
-const string = "Hello!";
-console.log(string.toUpperCase());
-console.log(string.toLowerCase());
-
-function shoutHELLO() {
-    console.log("HELLO!")
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string) {
+    let info;
+    if(string.toUpperCase()) {
+       info = "I can\'t hear you!"
+    }
+    else if(string.toUpperCase()){
+        info = "YES INDEED!"
+    }
+    else if (string ="Let\'s have dinner together!") {
+        info = "I would love to!"
+    }
+    return info;
 }
